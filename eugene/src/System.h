f50f6ff3be442ec8855adffc93c8d05d8f69a8d4
Thomas Schiex <Thomas.Schiex@inra.fr>
2000-01-31 14:54:57 +0000

    abandon des util glimmer

--- eugene/src/System.h
+++ eugene/src/System.h
@@ -2,53 +2,46 @@
 #define SYSTEM_H
 
 #define  TRUE  1
-#define  FALSE  0
+#define  FALSE 0
 
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 #define MAX(x, y) (((x) < (y)) ? (y) : (x))
 
-
-FILE *  File_Open  (const char * Filename, const char * Mode)
-
+inline FILE *FileOpen(const char *filename, const char *mode)
 {
-  FILE  *  fp;
+  FILE  *fp;
   
-  if  ((fp = fopen (Filename, Mode)) == NULL)
-    {
-      fprintf (stderr, "ERROR:  Could not open file  %s \n", Filename);
-      exit (1);
-    }  
+  if  (!(fp = fopen (filename, mode))) {
+    fprintf (stderr, "ERROR:  Could not open file  %s \n", filename);
+    exit (1);
+  }  
   return  fp;
-  }
+}
 
 // Malloc sur
-
-void *  Safe_malloc  (size_t Len)     
+inline void *  MyMalloc(size_t Len)     
 {
-  void  * P;
+  void  *Ptr;
   
-  
-  if  ((P = malloc (Len)) == NULL)
-    {
-      fprintf (stderr, "ERROR:  malloc failed\n");
-      exit (1);
-    }
-  return  P;
+  if  (!(Ptr = malloc (Len))) {
+    fprintf (stderr, "ERROR:  malloc failed\n");
+    exit (1);
+  }
+  return  Ptr;
 }
 
 // Realloc sur
 
-void *  Safe_realloc  (void * Q, size_t Len)
+inline void *  MyRealloc(void *Or, size_t Len)
 {
-  void  * P;
+  void  *Ptr;
   
-  
-  if  ((P = realloc (Q, Len)) == NULL)
-    {
-      fprintf (stderr, "ERROR:  realloc failed\n");
-      exit (1);
-    }  
-  return  P;
+  if  (!(Ptr = realloc (Or, Len))) {
+    fprintf (stderr, "ERROR:  realloc failed\n");
+    exit (1);
+  }  
+  return Ptr;
 }
+
 
 #endif
