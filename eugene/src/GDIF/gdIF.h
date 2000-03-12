fdf65e331c8794510f3827c09bb34fd8d458037c
tom <tom>
2000-03-12 13:54:36 +0000

    extern C vires

--- eugene/src/GDIF/gdIF.h
+++ eugene/src/GDIF/gdIF.h
@@ -1,7 +1,8 @@
-extern "C" void InitPNG(int x,int y, int From, int To,int olap, int ILen,char *name);
-extern "C" void PlotBarF(unsigned int nuc, signed char phase, REAL pos, REAL width, int col);
-extern "C" void PlotBarI(unsigned int nuc, signed char phase, REAL pos, int width, int col);
-extern "C" void PlotLine(unsigned int nuc1, unsigned int nuc2, 
-			 signed char phase1, signed char phase2, REAL pos1, REAL pos2, int col);
-extern "C" void ClosePNG();
-extern "C" void OutputHTMLFileNames();
+void InitPNG(int x,int y, int From, int To,int olap, int ILen,char *name);
+void PlotBarF(unsigned int nuc, signed char phase, REAL pos, REAL width, int col);
+void PlotBarI(unsigned int nuc, signed char phase, REAL pos, int width, int col);
+void PlotLine(unsigned int nuc1, unsigned int nuc2, 
+	      signed char phase1, signed char phase2, REAL pos1, REAL pos2, int col);
+void ClosePNG();
+void OutputHTMLFileNames();
+
