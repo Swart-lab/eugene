2ecbe5adcae43542e38be7a52f078cecc03218e7
Philippe Bardou <Philippe.Bardou@inra.fr>
2002-09-24 09:28:47 +0000

    Sensor user

---
+++ eugene/src/SensorPlugins/User/Sensor.User.cc
@@ -0,0 +1,55 @@
+#include "SensorUser.h"
+
+/*************************************************************
+ **                        SensorUser                       **
+ *************************************************************/
+
+extern Parameters PAR;
+
+// ----------------------
+//  Default constructor.
+// ----------------------
+SensorUser :: SensorUser ()
+{
+}
+
+// ----------------------
+//  Default destructor.
+// ----------------------
+SensorUser :: ~SensorUser ()
+{
+}
+
+// ----------------------
+//  Init user.
+// ----------------------
+void SensorUser :: Init (DNASeq *X)
+{
+  char tempname[FILENAME_MAX+1];
+  int errflag;
+
+  type = Type_Multiple;
+
+  fprintf(stderr,"Loading user data...");
+  
+  strcpy(tempname, PAR.fstname);
+  strcat(tempname, ".user");
+  errflag = Utilisateur(tempname);   //prise en compte de donnees utilisateur
+    
+  if (errflag) {
+    fprintf(stderr,"none found\n");
+  }
+  else fprintf(stderr,"done\n");
+}
+
+// -----------------------
+//  GiveInfo signal user.
+// -----------------------
+void SensorUser :: GiveInfo (DNASeq *X, int pos, DATA *d)
+{
+  UserInfoList = SignalUser;
+  Util(pos, UserInfoList, d);
+  
+  UserInfoList = ContentsUser;
+  Util(pos, UserInfoList, d);
+}
---
+++ eugene/src/SensorPlugins/User/Sensor.User.h
@@ -0,0 +1,22 @@
+#ifndef  SENSOR_USER_H_INCLUDED
+#define  SENSOR_USER_H_INCLUDED
+
+#include "Sensor.h"
+#include "structure.h"
+#include "yacc.tab.h"
+
+/*************************************************************
+ **                       SensorUser                        **
+ *************************************************************/
+class SensorUser : public Sensor
+{
+ private:
+
+ public:
+  SensorUser  ();
+  ~SensorUser ();
+  void Init     (DNASeq *);
+  void GiveInfo (DNASeq *, int, DATA *);
+};
+
+#endif
