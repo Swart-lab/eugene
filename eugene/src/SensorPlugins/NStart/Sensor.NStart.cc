db433a19ff0eece3a04037a3ccf2eec6d8a15469
Philippe Bardou <Philippe.Bardou@inra.fr>
2002-09-24 09:26:08 +0000

    Sensor netstart

---
+++ eugene/src/SensorPlugins/NStart/Sensor.NStart.cc
@@ -0,0 +1,97 @@
+#include "SensorNStart.h"
+
+/*************************************************************
+ **                       SensorNStart                      **
+ *************************************************************/
+
+extern Parameters PAR;
+
+// ----------------------
+// Default constructor.
+// ----------------------
+SensorNStart :: SensorNStart ()
+{
+  *Start = NULL;
+}
+
+// ----------------------
+//  Default destructor.
+// ----------------------
+SensorNStart :: ~SensorNStart ()
+{
+  delete [] Start[0];
+  delete [] Start[1];
+}
+
+// ----------------------
+//  Init start.
+// ----------------------
+void SensorNStart :: Init (DNASeq *X)
+{
+  char tempname[FILENAME_MAX+1];
+
+  type = Type_Start;
+
+  if(*Start != NULL) {
+    delete [] Start[0];
+    delete [] Start[1];
+  }
+
+  Start[0] = new REAL[X->SeqLen+1];
+  Start[1] = new REAL[X->SeqLen+1];
+  
+  fprintf(stderr, "Reading start file (NetStart).................");
+  fflush(stderr);
+  strcpy(tempname,PAR.fstname);
+  strcat(tempname,".starts");
+  ReadNStart(tempname, X->SeqLen, 0);
+  fprintf(stderr,"forward,");
+  fflush(stderr);
+  
+  strcpy(tempname,PAR.fstname);
+  strcat(tempname,".startsR");
+  ReadNStart(tempname, X->SeqLen, 1);
+  fprintf(stderr," reverse done\n");
+  
+  CheckStart(X, Start);
+}
+
+// ----------------------
+//  Read start file.
+// ----------------------
+void SensorNStart :: ReadNStart (char name[FILENAME_MAX+1], int Len, int rev)
+{
+  FILE *fp;
+  int i,j = -1;
+  double force;
+
+  if (!(fp = fopen(name, "r"))) {
+    fprintf(stderr, "cannot open start file %s\n", name);
+    exit(2);
+  }
+
+  for (i = 0; i<= Len; i++)
+    Start[rev][i] = 0.0;
+
+  while (1) {
+    i = fscanf(fp,"%d %lf %*s\n", &j, &force);
+    if (i == EOF) break;
+    if (i < 2) {
+      fprintf(stderr, "Error in start file %s, position %d\n", name, j);
+      exit(2);
+    }
+    if (rev) j = Len-j+2;
+    Start[rev][j-1] = pow(force,PAR.StartB)*(exp(-PAR.StartP));
+  }
+  if (j == -1) fprintf(stderr,"WARNING: empty NetStart file !\n");
+  fclose(fp);
+}
+
+// ------------------------
+//  GiveInfo signal start.
+// ------------------------
+void SensorNStart :: GiveInfo (DNASeq *X, int pos, DATA *d)
+{
+  d->Start[0] += Start[0][pos];
+  d->Start[1] += Start[1][pos];
+}
---
+++ eugene/src/SensorPlugins/NStart/Sensor.NStart.h
@@ -0,0 +1,22 @@
+#ifndef  SENSOR_NSTART_H_INCLUDED
+#define  SENSOR_NSTART_H_INCLUDED
+
+#include "Sensor.h"
+
+/*************************************************************
+ **                    SensorNStart                         **
+ *************************************************************/
+class SensorNStart : public Sensor
+{
+ private:
+  REAL *Start[2];
+  void ReadNStart (char[FILENAME_MAX+1], int, int);
+
+ public:
+  SensorNStart   ();
+  ~SensorNStart  ();
+  void Init     (DNASeq *);
+  void GiveInfo (DNASeq *, int, DATA *);
+};
+
+#endif
