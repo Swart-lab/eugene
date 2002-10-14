221c751cb33e8fc1801f7199282b849f692efaf9
Philippe Bardou <Philippe.Bardou@inra.fr>
2002-10-14 15:14:03 +0000

    Gestion des plugins (version simplifié)

--- eugene/src/Dll.cc
+++ eugene/src/Dll.cc
@@ -1,25 +1,31 @@
 #include <dlfcn.h>
 #include "Dll.h"
 
-DLLManager::DLLManager( const char *fname )
+DLLManager :: DLLManager(const char *fname, const char *factory=0)
 {
   // Try to open the library now and get any error message.
   h = dlopen( fname, RTLD_NOW );
   err = dlerror();
+  
+  // Try get the factory function if there is no error yet
+  factory_func=0;
+
+  if( LastError()==0 ) {		
+    GetSymbol( (void **)&factory_func, factory ? factory : "factory0" );
+  } 
 }
 
-DLLManager::~DLLManager()
+DLLManager :: ~DLLManager()
 {
-  // close the library if it isn't null
+  // Close the library if it isn't null
   if( h != 0 )
     dlclose(h);
 }
 
-
-bool DLLManager::GetSymbol(void **v, const char *sym_name)
+bool DLLManager :: GetSymbol(void **v, const char *sym_name)
 {
-  // try extract a symbol from the library
-  // get any error message is there is any
+  // Try extract a symbol from the library get any error message
+  // is there is any.
   if( h!=0 ) {
     *v = dlsym( h, sym_name );
     err=dlerror();
@@ -28,25 +34,6 @@ bool DLLManager::GetSymbol(void **v, const char *sym_name)
     else
       return false;
   }
-  else {	
+  else
     return false;
-  }
-}
-
-
-DLLFactoryBase::DLLFactoryBase(const char *fname,
-			       const char *factory=0
-			       ) : DLLManager(fname)
-{
-  // try get the factory function if there is no error yet
-  factory_func=0;
-  
-  if( LastError()==0 ) {		
-    GetSymbol( (void **)&factory_func, factory ? factory : "factory0" );
-  } 
-}
-
-
-DLLFactoryBase::~DLLFactoryBase()
-{
 }
