9f9bd4425a97d53c1f7776433eef759c33091e47
Philippe Bardou <Philippe.Bardou@inra.fr>
2002-10-18 14:29:09 +0000

    Modification CheckStart && CheckSplice (REAL ** -> vector)

--- eugene/src/Sensor.cc
+++ eugene/src/Sensor.cc
@@ -22,39 +22,39 @@ Sensor :: ~Sensor ()
 // ---------------------------------------
 //  Sanity check : check that ATG occurs.
 // ---------------------------------------
-void Sensor :: CheckStart(DNASeq *X, REAL **Start)
+void Sensor :: CheckStart(DNASeq *X, vector<int> vPosF, vector<int> vPosR)
 {
-  for (int pos = 0; pos <= X->SeqLen; pos++) {
-    if ((Start[0][pos] != 0.0) && (!X->IsEStart(pos,1)))
-      fprintf(stderr,"WARNING: Non ATG start predicted at %d on + strand!\n",
-	      pos+1);
-    
-    if ((Start[1][pos] != 0.0) && (!X->IsEStart(pos-1,-1)))
-      fprintf(stderr,"WARNING: Non ATG start predicted at %d on - strand!\n",
-	      pos+1);
-  }
+  for (int i = 0; i<(int)vPosF.size(); i++)
+    if (!X->IsEStart(vPosF[i],1))
+      fprintf(stderr,"WARNING: Non ATG start predicted at %d on + strand!\n", vPosF[i]+1);
+  
+  for (int i = 0; i<(int)vPosR.size(); i++)
+    if (!X->IsEStart(vPosR[i]-1,-1))
+      fprintf(stderr,"WARNING: Non ATG start predicted at %d on - strand!\n", vPosR[i]+1);
 }
 
 // -----------------------------------------------
 //  Sanity check; AG/GT or GC  splice site check.
 // -----------------------------------------------
-void Sensor :: CheckSplices (DNASeq *X, REAL **Acc, REAL **Don)
+void Sensor :: CheckSplices (DNASeq *X,
+			     vector<int> vPosAccF, vector<int> vPosDonF,
+			     vector<int> vPosAccR, vector<int> vPosDonR)
 {
-  for (int pos = 0; pos <= X->SeqLen; pos++) {
-    if (Acc[0][pos] != 0.0) 
-      if(((*X)[pos-2] != 'a') || ((*X)[pos-1] != 'g'))
-	fprintf(stderr,"WARNING: Non AG acceptor at %d (+ strand) !\n",pos);
-    
-    if (Acc[1][pos] != 0.0)
-      if(((*X)(pos) != 'g') || ((*X)(pos+1) != 'a'))
-	fprintf(stderr,"WARNING: Non AG acceptor at %d (- strand) !\n",pos);
-    
-    if (Don[0][pos] != 0.0)
-      if(((*X)[pos] != 'g') || (((*X)[pos+1] != 't') && ((*X)[pos+1] != 'c')))
-	fprintf(stderr,"WARNING: Non GT/GC donor at %d (+ strand) !\n",pos);
-    
-    if (Don[1][pos] != 0.0)
-      if((((*X)(pos-2) != 't') && ((*X)(pos-2) != 'c')) || ((*X)(pos-1)!='g'))
-	fprintf(stderr,"WARNING: Non GT/GC donor at %d (- strand) !\n",pos);
-  }
+  for (int i = 0; i<(int)vPosAccF.size(); i++)
+    if(((*X)[vPosAccF[i]-2] != 'a') || ((*X)[vPosAccF[i]-1] != 'g'))
+      fprintf(stderr,"WARNING: Non AG acceptor at %d (+ strand) !\n", vPosAccF[i]);
+  
+  for (int i = 0; i<(int)vPosAccR.size(); i++)
+    if(((*X)(vPosAccR[i]) != 'g') || ((*X)(vPosAccR[i]+1) != 'a'))
+      fprintf(stderr,"WARNING: Non AG acceptor at %d (- strand) !\n", vPosAccR[i]);
+  
+  for (int i = 0; i<(int)vPosDonF.size(); i++)
+    if(((*X)[vPosDonF[i]] != 'g') || 
+       (((*X)[vPosDonF[i]+1] != 't') && ((*X)[vPosDonF[i]+1] != 'c')))
+      fprintf(stderr,"WARNING: Non GT/GC donor at %d (+ strand) !\n", vPosDonF[i]);
+  
+  for (int i = 0; i<(int)vPosDonR.size(); i++)
+    if((((*X)(vPosDonR[i]-2) != 't') &&
+	((*X)(vPosDonR[i]-2) != 'c')) || ((*X)(vPosDonR[i]-1) != 'g'))
+      fprintf(stderr,"WARNING: Non GT/GC donor at %d (- strand) !\n", vPosDonR[i]);
 }
