2df153e830bd174eff86cc8307ac973a94827f89
Thomas Schiex <Thomas.Schiex@inra.fr>
2002-10-24 15:20:13 +0000

    Ajout lex/yacc/structure.h

---
+++ eugene/src/SensorPlugins/User/structure.h
@@ -0,0 +1,19 @@
+typedef struct UTIL *ptUTIL ;
+
+enum CHECK {check,nocheck};
+
+typedef struct UTIL
+{
+  int tab;
+  int   n1;
+  int   n2;
+  double  delta;
+  char* rais;
+  enum CHECK check;
+  ptUTIL suiv;
+} UTIL;
+
+extern int Utilisateur(char *nom_fich);
+extern ptUTIL SignalUser,ContentsUser,UserInfoList ;
+void Util(int i, ptUTIL ut, DATA *d);
+void WriteUtils(ptUTIL,FILE *);
