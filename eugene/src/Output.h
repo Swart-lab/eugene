56be46302a697b96c38591b04ab05b2c67abd5fc
Philippe Bardou <Philippe.Bardou@inra.fr>
2002-10-29 12:26:43 +0000

    Choice est éliminé. Arrivée de Prediction.

--- eugene/src/Output.h
+++ eugene/src/Output.h
@@ -12,13 +12,13 @@
 #include "DNASeq.h"
 #include "Hits.h"
 #include "Const.h"
+#include "Prediction.h"
 
-void Output (DNASeq *, char *Choice, int sequence, int argc, char * argv[]);
+void Output (DNASeq *, Prediction *, int sequence, int argc, char * argv[]);
 
 int HitsCompareLex(const void *, const void *);
 
 int IsPhaseOn(char, int);
-
 
 // Convertit les phases 0-6 en 1 2 3 -1 -2 -3 0
 int PhaseAdapt(char);
@@ -29,22 +29,13 @@ void PrintPhase(char);
 void DumpSignals(int Len, REAL** Donor, REAL **Acceptor,
 		 REAL** Stop, REAL **Start,FILE* flot);
 
-
-// Verif coherence EST: calcul le nombre de nuc. coherents et
-// incoherents avec les match est
-// debut/fin/etat: debut et fin de la seq. dont l'etat est etat
-// Match: resume des hits EST
-// cons/incons: retour des valeurs
-void CheckConsistency(int debut, int fin, int etat, 
-		      unsigned char *Match, int * cons, int* incons);
-
 // Verif coherence EST: calcul le nombre de nuc. coherents et
 // incoherents avec les match est
 // debut/fin/etat: debut et fin de la seq. dont l'etat est etat
 // cons/incons: retour des valeurs
 // WARNING : A modifier, utilise ESTMATCH_TMP (Cf struct DATA) !!!!!!!!!!!!
-void CheckConsistency2(int debut, int fin, int etat, 
-		       int * cons, int* incons, DNASeq *);
+void CheckConsistency(int debut, int fin, int etat, 
+		      int * cons, int* incons, DNASeq *);
 
 // Tdebut/fin = debut/fin de transcript
 // debut/fin = debut/fin de traduit
