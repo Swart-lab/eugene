a4de26a373469cb6a697667cd8e1fb3ea1da50a1
Philippe Bardou <Philippe.Bardou@inra.fr>
2002-10-31 16:24:03 +0000

    Ce sensor doit etre typé -> Type_Splice. Necessaire pour la verification des EST.

--- eugene/src/SensorPlugins/IfElse/Sensor.IfElse.cc
+++ eugene/src/SensorPlugins/IfElse/Sensor.IfElse.cc
@@ -20,7 +20,7 @@ SensorIfElse :: SensorIfElse (int n) : Sensor(n)
   sensorLIf =  new SensorLoader (sensorName);
 
   if (!sensorLIf->LastError()) {
-    fprintf(stderr,"Loading Sensor.%s, %d\n",PAR.getC("IfElse.SensorIf"),0);
+    fprintf(stderr," Loading Sensor.%s\t%d\n",PAR.getC("IfElse.SensorIf"),0);
     sensorIf = sensorLIf->MakeSensor(0);
   } else 
     fprintf(stderr,"WARNING: ignored plugin (invalid or not found) : %s\n",
@@ -31,7 +31,7 @@ SensorIfElse :: SensorIfElse (int n) : Sensor(n)
   strcat(sensorName, ".so");
   sensorLElse =  new SensorLoader (sensorName);
   if (!sensorLElse->LastError()) {
-    fprintf(stderr,"Loading Sensor.%s, %d\n",PAR.getC("IfElse.SensorElse"),0);
+    fprintf(stderr," Loading Sensor.%s\t%d\n",PAR.getC("IfElse.SensorElse"),0);
     sensorElse = sensorLElse->MakeSensor(0);
   } else 
     fprintf(stderr,"WARNING: ignored plugin (invalid or not found) : %s\n",
@@ -50,6 +50,7 @@ SensorIfElse :: ~SensorIfElse ()
 // ----------------------
 void SensorIfElse :: Init (DNASeq *X)
 {
+  type = Type_Splice;
   sensorIf->Init(X);
   sensorElse->Init(X);
 }
