9f4d829d5bcf68000b9823dbf18df56af5a5fe8c
Sylvain Foissac <Sylvain.Foissac@inra.fr>
2003-01-10 14:06:47 +0000

    Exploitation du nouveau DATA; utilisation des "DATA::Last" dans les boucles for.

--- eugene/src/SensorPlugins/IfElse/Sensor.IfElse.cc
+++ eugene/src/SensorPlugins/IfElse/Sensor.IfElse.cc
@@ -67,28 +67,28 @@ void SensorIfElse :: Init (DNASeq *X)
 void MergeInfo(DATA *ifData, DATA *elseData, DATA *d)
 {
   int i,j;
-  
+
   // on fusionne les signaux dans ifData
-  for (j=0; j <= DATA::Del; j++) {
-    for (i=0; i<= Signal::Reverse;  i++) 
+  for (j=0; j < DATA::LastSigType; j++) {
+    for (i=0; i<= Signal::Reverse;  i++)
       if (!ifData->sig[j].IsSet(i)) {
 	ifData->sig[j].weight[i] = elseData->sig[j].weight[i];
 	ifData->sig[j].weight[i+2] = elseData->sig[j].weight[i+2];
       }
   }
-  
+
   // on repercute dans d
-  for (j=0; j <= DATA::Del; j++) {
-    for (i=0; i<= Signal::Reverse;  i++) 
+  for (j=0; j < DATA::LastSigType; j++) {
+    for (i=0; i<= Signal::Reverse;  i++)
       if (ifData->sig[j].IsSet(i)) {
 	d->sig[j].weight[i] += ifData->sig[j].weight[i];
 	d->sig[j].weight[i+2] += ifData->sig[j].weight[i+2];
       }
   }
-  
-  for(i=0; i<13; i++) 
-    d->contents[i] += 
-      (ifData->contents[i] ? ifData->contents[i] : elseData->contents[i]);    
+
+  for(i=0; i< DATA::LastContentsType; i++)
+    d->contents[i] +=
+      (ifData->contents[i] ? ifData->contents[i] : elseData->contents[i]);
 }
 
 // -----------------------
@@ -100,15 +100,15 @@ void SensorIfElse :: GiveInfo (DNASeq *X, int pos, DATA *d)
   DATA elseData;
   int i;
 
-  for(i=0; i< DATA::Del+1;  i++)
-    ifData.sig[i].Clear();  
-  for(i=0; i<= DATA::UTR3R; i++) ifData.contents[i] = 0.0;
+  for(i=0; i<  DATA::LastSigType;  i++)
+    ifData.sig[i].Clear();
+  for(i=0; i< DATA::LastContentsType; i++) ifData.contents[i] = 0.0;
 
   sensorIf->GiveInfo(X,pos,&ifData);
 
-  for(i=0; i< DATA::Del+1;  i++)
-    elseData.sig[i].Clear();  
-  for(i=0; i<= DATA::UTR3R; i++) elseData.contents[i] = 0.0;
+  for(i=0; i<  DATA::LastSigType;  i++)
+    elseData.sig[i].Clear();
+  for(i=0; i< DATA::LastContentsType; i++) elseData.contents[i] = 0.0;
 
 
   sensorElse->GiveInfo(X,pos,&elseData);
