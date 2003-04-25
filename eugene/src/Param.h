5c6c561ce499544f6b03af78a14f764010c431e0
Sylvain Foissac <Sylvain.Foissac@inra.fr>
2003-04-25 09:51:40 +0000

    Actualisation de la version

--- eugene/src/Param.h
+++ eugene/src/Param.h
@@ -20,8 +20,8 @@
 #include "Const.h"
 #include "System.h"
 
-const char VERSION_PAR[FILENAME_MAX+1] = "10_01_03";
-const char VERSION[FILENAME_MAX+1]     = "1.50 (100103)";
+const char VERSION_PAR[FILENAME_MAX+1] = "25_04_03";
+const char VERSION[FILENAME_MAX+1]     = "1.55 (250403)";
 
 extern char   *optarg;   
 extern int     optind;
