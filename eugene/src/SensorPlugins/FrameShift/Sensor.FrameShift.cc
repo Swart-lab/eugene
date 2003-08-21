adda258d81a806af75742867fdae204cccbd3661
Philippe Bardou <Philippe.Bardou@inra.fr>
2003-08-21 15:31:27 +0000

    Debbug : insProb = exp(-PAR.getD... devient insProb = -exp(PAR.getD... delProb = exp(-PAR.getD... devient delProb = -exp(PAR.getD...
    Plot :
    Les frameshifts ce plottent : une ligne verticale rouge d'exon à exon

--- eugene/src/SensorPlugins/FrameShift/Sensor.FrameShift.cc
+++ eugene/src/SensorPlugins/FrameShift/Sensor.FrameShift.cc
@@ -27,8 +27,8 @@ SensorFrameShift :: ~SensorFrameShift ()
 void SensorFrameShift :: Init (DNASeq *X)
 {
   type = Type_Start;
-  insProb = exp(-PAR.getD("FrameShift.Ins"));
-  delProb = exp(-PAR.getD("FrameShift.Del"));
+  insProb = -exp(PAR.getD("FrameShift.Ins"));
+  delProb = -exp(PAR.getD("FrameShift.Del"));
   if (PAR.getI("Output.graph")) Plot(X);
 }
 
@@ -55,4 +55,28 @@ void SensorFrameShift :: Plot(DNASeq *X)
 // ------------------
 void SensorFrameShift :: PostAnalyse(Prediction *pred)
 {
+  int state, stateBack;
+  int posFs = -1;
+  
+  if (PAR.getI("Output.graph")) {
+    for(int i=pred->size()-1; i!=-1; i--) {
+      state = pred->getState(i);
+      if(state <= ExonR3)
+	if(posFs != -1) { // Frameshift plot
+	  if(state <= ExonF3)	 
+	    PlotLine(posFs, posFs, state+1, stateBack+1, 0.4, 0.4, 1);
+	  else
+	    PlotLine(posFs, posFs, -state+2, -stateBack+2, 0.4, 0.4, 1);
+	  
+	  posFs = pred->getPos(i);
+	  stateBack = state;
+	}
+	else {            // No frameshift
+	  posFs = pred->getPos(i);
+	  stateBack = state;
+	}
+      else                // Ig, UTR, or Intron 
+	posFs = -1;
+    }
+  }
 }
