85918801a17f69d05a20eff85a571ffe563beb7e
Marie-Josée Cros <Marie-Josee.Cros@inra.fr>
2004-05-11 10:04:06 +0000

    Remove the HTML output from the documentation because it is now too long.

--- eugene/doc/GenerateDoc.tcl
+++ eugene/doc/GenerateDoc.tcl
@@ -27,13 +27,12 @@ set CMDFLAGS_INDEX "CmdFlags"
 
 set Cmd_end " >& $FIC_TMP"
 set Flag(1) EXECUTION_TRACE1; set Cmd_begin(1) ""; set Cmd(1) "$EUGENEAS $SEQ -s"
-set Flag(2) EXECUTION_TRACE2; set Cmd_begin(2) ""; set Cmd(2) "$EUGENEAS $SEQ -s -ph"
-set Flag(3) EXECUTION_TRACE3; set Cmd_begin(3) ""; set Cmd(3) "$EUGENEAS $SEQ -s -d"
-set Flag(4) EXECUTION_TRACE4; set Cmd_begin(4) ""; set Cmd(4) "$EUGENEAS $SEQ -s -d -E"
-set Flag(5) EXECUTION_TRACE5; set Cmd_begin(5) ""; set Cmd(5) "$EUGENEAS $SEQ -s -d -b012"
-set Flag(6) EXECUTION_TRACE6; set Cmd_begin(6) "cp $SEQ.user1 $SEQ.user;"; set Cmd(6) "$EUGENEAS $SEQ -U"
-set Flag(7) EXECUTION_TRACE7; set Cmd_begin(7) "cp $SEQ.user2 $SEQ.user;"; set Cmd(7) "$EUGENEAS $SEQ -U"
-set nbflags 7
+set Flag(2) EXECUTION_TRACE2; set Cmd_begin(2) ""; set Cmd(2) "$EUGENEAS $SEQ -s -d"
+set Flag(3) EXECUTION_TRACE3; set Cmd_begin(3) ""; set Cmd(3) "$EUGENEAS $SEQ -s -d -E"
+set Flag(4) EXECUTION_TRACE4; set Cmd_begin(4) ""; set Cmd(4) "$EUGENEAS $SEQ -s -d -b012"
+set Flag(5) EXECUTION_TRACE5; set Cmd_begin(5) "cp $SEQ.user1 $SEQ.user;"; set Cmd(5) "$EUGENEAS $SEQ -U"
+set Flag(6) EXECUTION_TRACE6; set Cmd_begin(6) "cp $SEQ.user2 $SEQ.user;"; set Cmd(6) "$EUGENEAS $SEQ -U"
+set nbflags 6
 #===========================================================================
 
 
