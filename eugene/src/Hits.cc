e391224c672255160db3d10a6a126e1ead68c6f6
Philippe Bardou <Philippe.Bardou@inra.fr>
2004-07-20 13:36:26 +0000

    Ajout des champs Level et Support Modification de la methode ReadFromFile :  - lors d'appel repetes à la methode : ajout des hits à la suite  - ajout de param : 	- Level  (ex: blastx0 ...) 	- margin (marge pour les "bavures" des blast)  - gestion des phases<0

--- eugene/src/Hits.cc
+++ eugene/src/Hits.cc
@@ -1,15 +1,15 @@
-//
-//   T. Schiex
-//
-//     File:  Hits.cc
-//     Version:  1.0
-//
-//    Copyright (c) 2000 by Thomas Schiex All rights reserved.
-//    Redistribution is not permitted without the express written
-//    permission of the authors.
-//
-//  Definitions for a class representing alignements wrt to a seq.
-//
+/*****************************************************************************/
+/*             Copyright (c) 2004 by INRA. All rights reserved.              */
+/*                 Redistribution is not permitted  without                  */
+/*                 the express written permission of  INRA.                  */
+/*                   Mail : eugene@ossau.toulouse.inra.fr                    */
+/*---------------------------------------------------------------------------*/
+/* File         : EuGeneTk/EuGene/Hits.cc                                    */
+/* Description  : Definitions for a class representing alignements with      */
+/*                genomic seq.                                               */
+/* Authors      : P.Bardou, S.Foissac, M.J.Cros, A.Moisan, T.Schiex          */
+/* History      : July 2004                                                  */
+/*****************************************************************************/
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -31,20 +31,21 @@
 // ---------------------------------------------------------------------
 Block :: Block ()
 {
-  Prev =  Next = NULL;
+  Prev = Next = NULL;
 }
+
 // ---------------------------------------------------------------------
 //  Constuct from Deb/Fin
 // ---------------------------------------------------------------------
 Block ::  Block(int start, int end,int lstart,int lend,int Ph,int Scr)
 {
-  Start = start;
-  End = end;
+  Start  = start;
+  End    = end;
   LStart = lstart;
-  LEnd = lend;
-  Phase = Ph;
-  Score = Scr;
-  Prev = Next = NULL;
+  LEnd   = lend;
+  Phase  = Ph;
+  Score  = Scr;
+  Prev   = Next = NULL;
 }
 
 // ---------------------------------------------------------------------
@@ -52,10 +53,11 @@ Block ::  Block(int start, int end,int lstart,int lend,int Ph,int Scr)
 // ---------------------------------------------------------------------
 void Block ::  AddBlockAfter(int start,int end,int lstart,int lend,int Ph,int Scr)
 {
-  Block * ABlock = new Block(start,end,lstart,lend,Ph,Scr);
-  this->Next = ABlock;
-  ABlock->Prev = this;
+  Block *ABlock = new Block(start,end,lstart,lend,Ph,Scr);
+  this->Next    = ABlock;
+  ABlock->Prev  = this;
 }
+
 // ---------------------------------------------------------------------
 //  Destroy this Block and all blocks after him
 // ---------------------------------------------------------------------
@@ -63,80 +65,100 @@ Block :: ~ Block  ()
 {
   delete Next;
 }
+
 // ---------------------------------------------------------------------
 //  Default constructor.
 // ---------------------------------------------------------------------
 Hits :: Hits  ()
 {
   Rejected = 0;
-  Start = 0;
-  End = 0;
-  Evalue = 0.0;
-  Length = 0;
-  Strand = 0;
-  NGaps = 0;
-  Name = NULL;
-  Match = NULL;
-  Next = NULL;
+  Start    = 0;
+  End      = 0;
+  Evalue   = 0.0;
+  Level    = 0;
+  Support  = 0;
+  Length   = 0;
+  Strand   = 0;
+  NGaps    = 0;
+  Name     = NULL;
+  Match    = NULL;
+  Next     = NULL;
 }
 
 // ---------------------------------------------------------------------
 //  Construct from a char*...
 // ---------------------------------------------------------------------
-Hits :: Hits  (char* name, int length, char strand, int deb,int fin,int ldeb,int lfin,int Ph,int Scr,double Prob)
+Hits :: Hits  (char* name, int length, char strand, int deb, int fin, 
+	       int ldeb, int lfin, int Ph, int Scr, double Prob, int level,
+	       int sup)
 {
   Rejected = 0;
-  Name = new char[strlen(name)+1];
+  Name     = new char[strlen(name)+1];
   strcpy(Name,name);
   Strand = strand;
   Length = length;
-  Start = deb;
-  End = fin;
+  Start  = deb;
+  End    = fin;
   Evalue = Prob;
-  Match = new Block(deb,fin,ldeb,lfin,Ph,Scr);
-  NGaps = 0;
-  Next = NULL;
+  Level  = level;
+  Support= sup;
+  Match  = new Block(deb,fin,ldeb,lfin,Ph,Scr);
+  NGaps  = 0;
+  Next   = NULL;
 }
 
 // ---------------------------------------------------------------------
 //  Read a table of Hits from a file
 // ---------------------------------------------------------------------
-Hits* Hits::ReadFromFile(FILE* HitFile, int *NumHits) 
+Hits* Hits::ReadFromFile(FILE* HitFile, int *NumHits, int level, int margin) 
 {
-  char *HitId, *PHitId;
-  int deb,fin,phase,HSPDeb,HSPFin,poids,read;
-  double evalue,Pevalue;
-  char A[128],B[128];
+  char   *HitId, *PHitId;
+  int    deb, fin, phase, HSPDeb, HSPFin, poids, read;
+  double evalue, Pevalue;
+  char   A[128], B[128];
   Block *ThisBlock = NULL;
-  Hits *OneHit = NULL, *ThisHit = NULL, *AllHit = NULL;
-
+  Hits  *OneHit    = NULL, *ThisHit = this, *AllHit = this;
+  
   Pevalue = -1.0;
-  A[0] = B[0]= 0;
-  HitId = A;
-  PHitId = B;
+  A[0]    = B[0] = 0;
+  HitId   = A;
+  PHitId  = B;
+  
+  if (ThisHit != NULL)
+    for (int i=0; i<*NumHits-1; i++) ThisHit = ThisHit->Next;
 
-  while ((read=fscanf(HitFile,"%d %d %d %lf %d %s %d %d\n",
-		      &deb, &fin,&poids,&evalue,&phase,HitId,&HSPDeb,&HSPFin)) == 8)
+  while ((read=fscanf(HitFile,"%d %d %d %lf %d %s %d %d\n", &deb, &fin, 
+		      &poids, &evalue, &phase, HitId, &HSPDeb, &HSPFin)) == 8)
     {
-      if ((strcmp(HitId,PHitId) == 0) && 
-	  (HSPDeb > ThisBlock->LEnd) && 
-	  (deb > ThisBlock->End) &&
-	  (evalue == Pevalue))
+      if (phase < 0) {
+	int tmp = deb;
+	deb     = fin;
+	fin     = tmp;
+	tmp     = HSPDeb;
+	HSPDeb  = HSPFin;
+	HSPFin  = tmp;
+      }
+      if ((strcmp(HitId,PHitId) == 0)      &&
+	  (deb + margin > ThisBlock->End)  &&  evalue == Pevalue  &&
+	  (phase >= 0  &&  (HSPDeb + margin > ThisBlock->LEnd)    ||
+	   phase < 0   &&  (HSPDeb - margin < ThisBlock->LEnd)))
 	// si HitId et PHitId sont égaux, alors il y a un Hit en cours
 	// de meme nom on verifie que c'est bien compatible en terme
-	// de position (sur l'est et le genomique) et en e-value.
-	  {
-	    ThisHit->NGaps++;
-	    ThisHit->End = fin-1;
-	    ThisBlock->AddBlockAfter(deb-1,fin-1,HSPDeb,HSPFin,phase,poids);
-	    ThisBlock = ThisBlock->Next;
-	  }
+	// de position (sur l'est et le genomique) et en e-value, en 
+	// prenant en compte le brin !
+	{
+	  ThisHit->NGaps++;
+	  ThisHit->End = fin-1;
+	  ThisBlock->AddBlockAfter(deb-1,fin-1,HSPDeb,HSPFin,phase,poids);
+	  ThisBlock = ThisBlock->Next;
+	}
       else {
 	(*NumHits)++;
-	OneHit = new Hits(HitId,poids,(phase>0 ? 1 : -1),deb-1,fin-1,HSPDeb,HSPFin,phase,poids,evalue);
+	OneHit = new Hits(HitId, poids, (phase>0 ? 1 : -1), deb-1, fin-1,
+			  HSPDeb, HSPFin, phase, poids, evalue, level, 0);
 	ThisBlock = OneHit->Match;
-	PHitId = HitId;
-	Pevalue = evalue;
+	PHitId    = HitId;
+	Pevalue   = evalue;
 
 	if (AllHit == NULL) {
 	  AllHit = OneHit;
@@ -144,7 +166,7 @@ Hits* Hits::ReadFromFile(FILE* HitFile, int *NumHits)
 	}
 	else {
 	  ThisHit->Next = OneHit;
-	  ThisHit = OneHit;
+	  ThisHit       = OneHit;
 	}
       }
     }
@@ -152,6 +174,7 @@ Hits* Hits::ReadFromFile(FILE* HitFile, int *NumHits)
 
   return AllHit;
 }
+
 // ---------------------------------------------------------------------
 //  Destroy this alignement
 // ---------------------------------------------------------------------
@@ -161,4 +184,3 @@ Hits :: ~ Hits  ()
   delete Name;
   delete Next;
 }
-
--- eugene/src/Hits.h
+++ eugene/src/Hits.h
@@ -1,30 +1,27 @@
-// ---------------------------------------------------------------
-//   T. Schiex
-//
-//     File:  Hits.h
-//     Version:  1.0
-//
-//    Copyright (c) 2000 by Thomas Schiex All rights reserved.
-//    Redistribution is not permitted without the express written
-//    permission of the authors.
-//
-//  Definitions for a class representing alignements with genomic seq.
-// ---------------------------------------------------------------
-
+/*****************************************************************************/
+/*             Copyright (c) 2004 by INRA. All rights reserved.              */
+/*                 Redistribution is not permitted  without                  */
+/*                 the express written permission of  INRA.                  */
+/*                   Mail : eugene@ossau.toulouse.inra.fr                    */
+/*---------------------------------------------------------------------------*/
+/* File         : EuGeneTk/EuGene/Hits.h                                     */
+/* Description  : Definitions for a class representing alignements with      */
+/*                genomic seq.                                               */
+/* Authors      : P.Bardou, S.Foissac, M.J.Cros, A.Moisan, T.Schiex          */
+/* History      : July 2004                                                  */
+/*****************************************************************************/
 
 #ifndef  HITS_H_INCLUDED
 #define  HITS_H_INCLUDED
 #include <stdio.h>
 
-
 class Block
 {
  private:
-
+  
  public:
-
   Block ();
-  Block(int Start, int End,int LStart,int LEnd,int Ph,int Scr);
+  Block(int Start, int End, int LStart, int LEnd, int Ph, int Scr);
   ~Block ();
   
   void AddBlockAfter(int Start,int End,int LStart,int LEnd,int Ph,int Scr);
@@ -36,7 +33,6 @@ class Block
   int LStart;
   int LEnd;
   Block *Prev,*Next;
-  
 };
 
 class  Hits
@@ -45,22 +41,23 @@ class  Hits
 
  public:
   Hits ();
-  Hits  (char* name, int length, char strand,int deb,int fin,int ldeb,int lfin,
-	 int Ph,int Scr,double prob);
-  Hits* Hits::ReadFromFile(FILE* HitFile,int * NumSeqs);
-  
+  Hits (char* name, int length, char strand, int deb, int fin, int ldeb,
+	int lfin, int Ph, int Scr, double prob, int level, int sup);
+  Hits* Hits::ReadFromFile(FILE* HitFile, int *NumSeqs, int level, int margin);
   ~Hits ();
 
-  char *Name;
-  char Strand;
-  int Length;
-  int NGaps;
-  int Start;
-  int End;
+  char   *Name;
+  char   Strand;
+  int    Length;
+  int    NGaps;
+  int    Start;
+  int    End;
   double Evalue;
-  char Rejected;
+  char   Rejected;
+  int    Level;         // For blastx level 0-9 
+  int    Support;
 
   Block *Match;
-  Hits *Next;
+  Hits  *Next;
 };
 #endif
