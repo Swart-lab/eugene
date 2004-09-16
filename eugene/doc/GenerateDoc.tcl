433a554af7ab30eca4bc3f36ac037d848bf20b9e
Marie-Josée Cros <Marie-Josee.Cros@inra.fr>
2004-09-16 12:41:21 +0000

    Autoconfiscation: * eugene instead of EuGeneAS, eugene instead of EuGeneTk * version number and organism are parameters in the parameter file * all the documentation files (including the pluging ones) are in the doc directory

--- eugene/doc/GenerateDoc.tcl
+++ eugene/doc/GenerateDoc.tcl
@@ -1,14 +1,23 @@
 #!/usr/bin/tclsh
-#============================================================================
-#=             Copyright (c) 2002 by INRA. All rights reserved.             
-#=                 Redistribution is not permitted without                  
-#=                 the express written permission of INRA.                 
-#=                     Mail : tschiex@toulouse.inra.fr                     
-#=-------------------------------------------------------------------------
-#= File         : EuGeneTk/Documentation/GenerateDoc.tcl               
-#= Description  : Generation of the EuGene documentation
-#= Authors      : P.Bardou, S.Foissac, M.J.Cros, A.Moisan, T.Schiex         
-#===========================================================================
+
+# ------------------------------------------------------------------
+# Copyright (C) 2004 INRA <eugene@ossau.toulouse.inra.fr>
+#
+# This program is open source; you can redistribute it and/or modify
+# it under the terms of the Artistic License (see LICENSE file).
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+#
+# You should have received a copy of Artistic License along with
+# this program; if not, please see http://www.opensource.org
+#
+#  $Id$
+# ------------------------------------------------------------------
+# File:     GenerateDoc.tcl
+# Contents: Generation of the eugene documentation
+# ------------------------------------------------------------------
 
 
 
@@ -16,9 +25,9 @@
 # definition of variables related to I/O
 
 set SEQ Sequences/SYNO_ARATH.fasta
-set EUGENEAS_DIR ../EuGene/
-set EUGENEAS EuGeneAS 
-exec cp ../EuGene/EuGeneAS.par .
+set EUGENE_DIR ../src/
+set EUGENE eugene
+exec cp ${EUGENE_DIR}${EUGENE}.par .
 
 set FIC_TEX "Doc.tex"
 set FIC_TEX_TMP "EuGeneDoc"
@@ -26,12 +35,12 @@ set FIC_TMP "tmp"
 set CMDFLAGS_INDEX "CmdFlags"
 
 set Cmd_end " >& $FIC_TMP"
-set Flag(1) EXECUTION_TRACE1; set Cmd_begin(1) ""; set Cmd(1) "$EUGENEAS $SEQ -s"
-set Flag(2) EXECUTION_TRACE2; set Cmd_begin(2) ""; set Cmd(2) "$EUGENEAS $SEQ -s -d"
-set Flag(3) EXECUTION_TRACE3; set Cmd_begin(3) ""; set Cmd(3) "$EUGENEAS $SEQ -s -d -E"
-set Flag(4) EXECUTION_TRACE4; set Cmd_begin(4) ""; set Cmd(4) "$EUGENEAS $SEQ -s -d -b012"
-set Flag(5) EXECUTION_TRACE5; set Cmd_begin(5) "cp $SEQ.user1 $SEQ.user;"; set Cmd(5) "$EUGENEAS $SEQ -U"
-set Flag(6) EXECUTION_TRACE6; set Cmd_begin(6) "cp $SEQ.user2 $SEQ.user;"; set Cmd(6) "$EUGENEAS $SEQ -U"
+set Flag(1) EXECUTION_TRACE1; set Cmd_begin(1) ""; set Cmd(1) "$EUGENE $SEQ -s"
+set Flag(2) EXECUTION_TRACE2; set Cmd_begin(2) ""; set Cmd(2) "$EUGENE $SEQ -s -d"
+set Flag(3) EXECUTION_TRACE3; set Cmd_begin(3) ""; set Cmd(3) "$EUGENE $SEQ -s -d -E"
+set Flag(4) EXECUTION_TRACE4; set Cmd_begin(4) ""; set Cmd(4) "$EUGENE $SEQ -s -d -b012"
+set Flag(5) EXECUTION_TRACE5; set Cmd_begin(5) "cp $SEQ.user1 $SEQ.user;"; set Cmd(5) "$EUGENE $SEQ -U"
+set Flag(6) EXECUTION_TRACE6; set Cmd_begin(6) "cp $SEQ.user2 $SEQ.user;"; set Cmd(6) "$EUGENE $SEQ -U"
 set nbflags 6
 #===========================================================================
 
@@ -59,7 +68,7 @@ for {set i 1} {$i<= $nbflags} {incr i} {
 	    eval exec $Cmd_begin($i)
 	}
 
-	eval exec $EUGENEAS_DIR$Cmd($i) $Cmd_end
+	eval exec $EUGENE_DIR$Cmd($i) $Cmd_end
 
 	# write the executed command in the documentation before the result
 	set new_content "$new_content >$Cmd($i)\n"
@@ -71,15 +80,15 @@ for {set i 1} {$i<= $nbflags} {incr i} {
 set new_content "$new_content $content"
 
 # write new tex doc file
-set f [open $FIC_TEX_TMP w]
+set f [open $FIC_TEX_TMP.tex w]
 puts $f $new_content
 close $f
 
 # clean directory (beware except image .png)
-exec rm EuGeneAS.par  $FIC_TMP
+exec rm ${EUGENE}.par  $FIC_TMP
 
 # ask for compilation
-exec pdflatex $FIC_TEX_TMP
+exec pdflatex $FIC_TEX_TMP.tex
 catch {exec makeindex $CMDFLAGS_INDEX}
-exec pdflatex $FIC_TEX_TMP
-puts "EuGène documentation generated."
+exec pdflatex $FIC_TEX_TMP.tex
+puts "eugene documentation generated."
