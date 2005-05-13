298e4884d1208e58f05b7af70152946814715b08
Thomas Schiex <Thomas.Schiex@inra.fr>
2005-05-13 11:24:44 +0000

    Oops. Better header

--- eugene/src/DAG.h
+++ eugene/src/DAG.h
@@ -1,3 +1,22 @@
+// ------------------------------------------------------------------
+// Copyright (C) 2004 INRA <eugene@ossau.toulouse.inra.fr>
+//
+// This program is open source; you can redistribute it and/or modify
+// it under the terms of the Artistic License (see LICENSE file).
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+//
+// You should have received a copy of Artistic License along with
+// this program; if not, please see http://www.opensource.org
+//
+// $Id$
+// ------------------------------------------------------------------
+// File:     DAG.h
+// Contents: Modeling genes as a DAG and exploring it - Header
+// ------------------------------------------------------------------
+
 #ifndef DAG_H_INCLUDED
 #define DAG_H_INCLUDED
 #endif
