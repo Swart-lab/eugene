23bad77dc0ccc2b30a4a3a2f25da6d745576a238
Thomas Schiex <Thomas.Schiex@inra.fr>
2008-03-28 14:18:52 +0000

    Using the new linenumber argument + minor memory cleanup

--- eugene/src/GeneFeature.cc
+++ eugene/src/GeneFeature.cc
@@ -39,7 +39,7 @@ GeneFeature::GeneFeature()
 
 //Constructor from a gff3 line
 // prerequis : no \n at the end of the line.
-GeneFeature::GeneFeature ( char * line) 
+GeneFeature::GeneFeature ( char * line, int lineno) 
 {
   id_=".";
   seqid_=".";
@@ -52,7 +52,6 @@ GeneFeature::GeneFeature ( char * line)
   valid_= true ;
   length_=0;
   
-  valid_= true ;
   string line_str =to_string(line);
   //chomp : 
   size_t pos=line_str.find("\n") ;
@@ -63,7 +62,7 @@ GeneFeature::GeneFeature ( char * line)
   
   if ( line[0] != '#' )
   {  
-    ParseLine(line); 
+    ParseLine(line, lineno); 
   }
 }
 
@@ -95,11 +94,11 @@ GeneFeature::GeneFeature ( const GeneFeature & gene)
 //  ParseLine : Parse a gff3 line with a token, check SO/SOFA code and Parent Attribut.
 // ---------------------------------------
 
-void GeneFeature::ParseLine ( char * line ) 
+void GeneFeature::ParseLine ( char * line, int linenum ) 
 {
   
   int size = strlen(line);
-  char * token=new char [size+1];
+  char * token;
   char delims[] = "\t";
   token = strtok (line,delims);
   int i =0;
@@ -128,7 +127,7 @@ void GeneFeature::ParseLine ( char * line )
 	type_= to_string (token);
 	if (  ( ! GeneFeatureSet::soTerms_->existsId(type_) ) && ( !GeneFeatureSet::soTerms_->existsName(type_) ) )  
 	{
-	  cout << "WARNING : " << type_ << " is not SO/SOFA referenced terms"<<endl;
+	  cout << "WARNING : " << type_ << " is not SO/SOFA referenced terms in line " << linenum << endl;
 	  valid_=false;
 	}
 	break;
@@ -173,7 +172,7 @@ void GeneFeature::ParseLine ( char * line )
 	id_=attributes_->getId();
 	if ( id_ == "" )
 	{
-	  cout <<"WARNING : Feature has no ID >" << id_ << "<"<<endl;
+	  cout <<"WARNING : Feature has no ID in line " << linenum <<endl;
 	  valid_=false;
 	}
 	delete [] attributeString;
@@ -186,8 +185,10 @@ void GeneFeature::ParseLine ( char * line )
   }
   locus_  = new Locus (start, end, strand);
   length_ = end-start+1;
-  delete [] token;
-
+  if (i < 9) {
+    cout <<"WARNING : incomplete GFF feature in line " << linenum <<endl;
+    valid_ = false;
+  }
 }
 
 
