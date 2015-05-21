94cd301133013fc71b097e4441f269082df0367f
Erika Sallet <Erika.Sallet@inra.fr>
2015-05-21 13:38:56 +0000

    WAM directory.

--- eugene/src/Const.h
+++ eugene/src/Const.h
@@ -32,6 +32,7 @@
 #define DEFAULT_PARA_PROK_FILE "cfg/eugene_prok.par"
 #define PLUGINS_DIR "plugins"
 #define MODELS_DIR "models"
+#define WAM_DIR "models/WAM"
 #define WEB_DIR "web"
 
 #define POSSIBLE_ARGUMENTS "FGREBP::adrsghm:w:f:n:o:p:x:y:c:u:v:U:V:b::l:O:D:t::M:Z::A:"
